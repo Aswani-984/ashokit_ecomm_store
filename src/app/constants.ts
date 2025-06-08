29432cd7ccccc96430dfbdfc890ecf4e12a4d2e0
Aswani-984 <aswanivaleti7@gmail.com>
2025-06-08 19:05:46 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a704cc94d0d7649da8a5484c373c0106-283151249.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a1bc16d4751b14c64a3c36d5e6a8963f-1569588314.ap-south-1.elb.amazonaws.com',
 };
